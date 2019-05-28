44493690f9683627254544952a3e45b057aa3edf
Prahesa K Setia <prahesa@yahoo.com>
2019-05-29 03:24:47 +0700

    complete initializer function

--- shamir.go
+++ shamir.go
@@ -1,18 +1,44 @@
-package shamirss
+package shamirssgo
 
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+const PrimeCertainty = 128
 
 type ShamirSecret struct {
-	secret            big.Int
+	secret            *big.Int
 	threshold         int
-	modulus           big.Int
-	reconstructVector []big.Int
+	modulus           *big.Int
+	reconstructVector []*big.Int
 }
 
-func New(secret big.Int, threshold int, modulus big.Int) *ShamirSecret {
+func New(secret *big.Int, threshold int, modulus *big.Int) *ShamirSecret {
+	if !modulus.ProbablyPrime(PrimeCertainty) {
+		panic("modulus fails prime test by ProbablePrime(), try to use prime (or prime enough)")
+	}
+
+	modulusBitLen := modulus.BitLen()
+	max := new(big.Int)
+	max.Exp(
+		big.NewInt(2),
+		big.NewInt(int64(modulusBitLen)),
+		nil,
+	).Sub(max, big.NewInt(1))
+
+	reconstructVector := make([]*big.Int, threshold)
+	reconstructVector[0] = big.NewInt(1)
+
+	for i := 1; i < threshold-1; i++ {
+		randNum, _ := rand.Int(rand.Reader, max)
+		reconstructVector[i] = randNum.Mod(randNum, modulus)
+	}
+
 	return &ShamirSecret{
-		secret:    secret,
-		threshold: threshold,
-		modulus:   modulus,
+		secret:            secret.Mod(secret, modulus),
+		threshold:         threshold,
+		modulus:           modulus,
+		reconstructVector: reconstructVector,
 	}
 }
--- shamir_test.go
+++ shamir_test.go
@@ -1,9 +1,13 @@
-package shamirss_test
+package shamirssgo_test
 
 import (
+	"math/big"
 	"testing"
+
+	"github.com/hesahesa/shamirssgo"
 )
 
 func TestShamir(t *testing.T) {
-
+	secret := big.NewInt(100)
+	shamirssgo.New(secret)
 }
